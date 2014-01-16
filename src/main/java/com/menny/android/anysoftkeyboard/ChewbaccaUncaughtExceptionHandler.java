f2eecb4ad34ecfd572e85a70001600357eef955a
Menny Even Danan <menny@evendanan.net>
2014-01-15 22:39:29 -0500

    not Chewbacca in release mode

--- src/main/java/com/menny/android/anysoftkeyboard/ChewbaccaUncaughtExceptionHandler.java
+++ src/main/java/com/menny/android/anysoftkeyboard/ChewbaccaUncaughtExceptionHandler.java
@@ -48,7 +48,7 @@ class ChewbaccaUncaughtExceptionHandler implements UncaughtExceptionHandler {
 
     public void uncaughtException(Thread thread, Throwable ex) {
         Log.e(TAG, "Caught an unhandled exception!!! ", ex);
-        boolean ignore = false;
+        boolean ignore = !BuildConfig.DEBUG;
 
         // https://github.com/AnySoftKeyboard/AnySoftKeyboard/issues/15
         String stackTrace = Log.getStackTrace(ex);
